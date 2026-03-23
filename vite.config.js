8b6b3b9f46b3607106fb91458f891023285a3220
Adam Elmekadem <adamElmekadem61@gmail.com>
2026-03-23 14:02:22 +0100

    fix: update build command for SPA target and conditionally include vike plugin

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import vike from "vike/plugin";
 
+const isSpaBuild = process.env.BUILD_TARGET === 'spa'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), vike()],
+  plugins: [react(), tailwindcss(), ...(isSpaBuild ? [] : [vike()])],
 });
